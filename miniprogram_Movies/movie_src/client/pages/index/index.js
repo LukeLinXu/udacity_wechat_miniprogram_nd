d77de6588fbd38be8fba5787902bcdbc6d035328
LukeLin <dev.linxu@gmail.com>
2018-08-26 07:15:41 -0400

    add link to ReviewDetail from Index

--- miniprogram_Movies/movie_src/client/pages/index/index.js
+++ miniprogram_Movies/movie_src/client/pages/index/index.js
@@ -51,6 +51,13 @@ Page({
     })
   },
 
+  onTapAvatar: function (event){
+    let id = event.currentTarget.dataset.id
+    wx.navigateTo({
+      url: '/pages/reviewDetail/reviewDetail?comment_id=' + id,
+    })
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
