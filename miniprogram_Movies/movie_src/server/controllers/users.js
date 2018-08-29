39b2f4bd82d1eb5c3128b63b6dc58bb44d2d2982
LukeLin <dev.linxu@gmail.com>
2018-08-29 10:16:09 -0400

    add getUserDetail api.

--- miniprogram_Movies/movie_src/server/controllers/users.js
+++ miniprogram_Movies/movie_src/server/controllers/users.js
@@ -3,9 +3,17 @@ module.exports = {
   detail: async ctx => {
     let userId = ctx.params.id
     let user
+    let likesDetail
     
     if (userId) {
       user = (await DB.query('select * from users where users.id = ?', [userId]))[0]
+      likesDetail = (await DB.query('select * from comments where comments.id in ('+user.likes+')'))
+      for(i  = 0; i < likesDetail.length; i++){
+          let comment = likesDetail[i]
+          comment.movie = (await DB.query('select * from movies where movies.id = ?', [comment.movie_id]))[0];
+          comment.user = (await DB.query('select * from users where users.id = ?', [comment.user_id]))[0];
+      }
+      user.likesDetail = likesDetail
     } else {
       user = {}
     }
