57a496a74449bfa56507da487de8890f899db746
LukeLin <dev.linxu@gmail.com>
2018-08-29 11:00:12 -0400

    add showActionSheet

--- miniprogram_Movies/movie_src/client/pages/reviewDetail/reviewDetail.js
+++ miniprogram_Movies/movie_src/client/pages/reviewDetail/reviewDetail.js
@@ -49,52 +49,66 @@ Page({
       })
   },
 
+  openCommentOptions(e){
+    wx.showActionSheet({
+      itemList: ['文字', '音频'],
+      itemColor: '#007aff',
+      success(res) {
+        if (res.tapIndex === 0) {
+          wx.showToast({ title: '文字成功！' });
+        } else if (res.tapIndex === 1) {
+          wx.showToast({ title: '音频成功！' });
+        }
+      }
+    })
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
