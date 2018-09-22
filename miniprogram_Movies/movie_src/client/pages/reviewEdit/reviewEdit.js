ae7c7607e7984b8d12bfcfc317930f62611b45aa
LukeLin <dev.linxu@gmail.com>
2018-09-21 22:51:00 -0400

    done add comment feature on client side.

--- miniprogram_Movies/movie_src/client/pages/reviewEdit/reviewEdit.js
+++ miniprogram_Movies/movie_src/client/pages/reviewEdit/reviewEdit.js
@@ -1,6 +1,18 @@
 // pages/reviewEdit/reviewEdit.js
 const qcloud = require('../../vendor/wafer2-client-sdk/index.js')
 const config = require('../../config.js')
+const app = getApp()
+// recordStatus
+const UNRECORDED = 0
+const RECORDING = 1
+const RECORDED = 2
+
+// recordAuthStatus
+const UNVERIFIED = 0
+const UNAUTHORIZED = 1
+const AUTHORIZED = 2
+
+const recorderManager = wx.getRecorderManager()
 
 Page({
 
@@ -8,9 +20,13 @@ Page({
    * 页面的初始数据
    */
   data: {
+      recordStatusText: ['长按录音', '松开停止', '重新录音'],
       type: 0,
       movie: null,
-    inputValue: null
+      inputValue: "",
+      duration: 0,
+      recordStatus: UNRECORDED,
+      recordAuthStatus: UNVERIFIED
   },
 
   /**
@@ -19,7 +35,7 @@ Page({
   onLoad: function (options) {
       this.setData({
         movie: JSON.parse(options.movie),
-        type: options.type_id
+        type: parseInt(options.type_id)
       })
   },
 
@@ -30,13 +46,110 @@ Page({
   },
 
   onSubmitClick: function() {
-    let commentObj = this.data.inputValue
-    let movieObj = JSON.stringify(this.data.movie)
-    let type = this.data.type
+    let comment = {}
+    let dataObj = {}
+      dataObj.type = this.data.type
+      dataObj.movie = this.data.movie
+      comment.content = this.data.inputValue
+      comment.duration = this.data.duration
+      dataObj.comment = comment
+      console.log(dataObj)
     wx.navigateTo({
-      url: '/pages/reviewPreview/reviewPreview?type=type'+'&comment='+commentObj+'&movie='+movieObj
+      url: '/pages/reviewPreview/reviewPreview?dataObj='+encodeURIComponent(JSON.stringify(dataObj))
     })
   },
+
+
+    getRecordAuth(forceAuth) {
+        wx.getSetting({
+            success: (res) => {
+                let auth = res.authSetting['scope.record']
+                console.log(auth)
+                if (auth === undefined) {
+                    if (forceAuth) {
+                        wx.authorize({
+                            scope: 'scope.record',
+                            success: () => {
+                                this.setData({
+                                    recordAuthStatus: AUTHORIZED
+                                })
+                            },
+                            fail: error => {
+                                console.log(error)
+                                this.setData({
+                                    recordAuthStatus: UNAUTHORIZED
+                                })
+                            }
+                        })
+                    }
+                } else if (auth === false) {
+                    this.setData({
+                        recordAuthStatus: UNAUTHORIZED
+                    })
+                } else if (auth === true) {
+                    this.setData({
+                        recordAuthStatus: AUTHORIZED
+                    })
+                }
+            },
+            fail: error => {
+                console.log(error)
+            }
+        })
+    },
+
+
+    settingCallBack(event) {
+        console.log(event)
+        let auth = event.detail.authSetting['scope.record']
+
+        if (auth === false) {
+            this.setData({
+                recordAuthStatus: UNAUTHORIZED
+            })
+        } else {
+            this.setData({
+                recordAuthStatus: AUTHORIZED
+            })
+        }
+    },
+
+    startRecord() {
+        if (this.data.recordAuthStatus === UNVERIFIED) {
+            this.getRecordAuth(true)
+        } else if (this.data.recordAuthStatus === AUTHORIZED) {
+            recorderManager.onStart(() => {
+                wx.vibrateShort();
+                this.setData({
+                    recordStatus: RECORDING,
+                })
+            })
+            const options = {
+                duration: 60000,
+                sampleRate: 44100,
+                numberOfChannels: 1,
+                encodeBitRate: 192000,
+                format: 'mp3',
+                frameSize: 50
+            }
+            recorderManager.start(options)
+        }
+    },
+
+    stopRecord() {
+      if(this.data.recordAuthStatus == AUTHORIZED){
+          recorderManager.stop()
+          recorderManager.onStop((res) => {
+              console.log(res)
+              this.setData({
+                  duration: parseInt(res.duration),
+                  recordStatus: RECORDED,
+                  inputValue: res.tempFilePath
+              })
+          })
+      }
+        // }
+    },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -49,7 +162,13 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+      if(app.getUserInfo()){
+          this.getRecordAuth(false);
+      }else {
+          wx.navigateTo({
+              url: '/pages/login/login'
+          })
+      }
   },
 
   /**
